31b62f32ef40b3e57d6e3239d452f9e8301a35c0
nikamharshad347@gmail.com <nikamharshad347@gmail.com>
2023-05-23 08:54:32 +0530

    Reimbursement Get or Delete Operations

--- ProjectOperation/src/main/java/com/ecom/service/ReimbursementService.java
+++ ProjectOperation/src/main/java/com/ecom/service/ReimbursementService.java
@@ -3,8 +3,10 @@ package com.ecom.service;
 import com.ecom.model.Reimbursement;
 
 public interface ReimbursementService {
-	
-	
+
 	public Reimbursement saveReimbursement(Reimbursement reimbursement);
 
+	public Reimbursement getReimbursement(int reimbursementId);
+
+	public void deleteReimbursement(int reimbursementId);
 }
